7ae8954d9a51274242a2999f4bb46c638a6fa0da
JR <janakiraman@proisc.com>
2016-02-17 22:33:07 +0530

    Added the tweet functionality

--- learn/learn/settings.py
+++ learn/learn/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 	'users.apps.UsersConfig',
+	'tweets.apps.TweetsConfig',
 	'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
